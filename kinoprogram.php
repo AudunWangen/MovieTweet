c4c8c421939b8df3a0dc1aad5452e90ad1b2085a
Audun Wangen <awa7093@lt258ikt.kk.lokal>
2010-07-28 17:05:54 +0200

    Initial commit without passwords this time, hopefully.

---
+++ kinoprogram.php
@@ -0,0 +1,254 @@
+#!/usr/bin/php -q
+<?php
+/**
+ * xml2array() will convert the given XML text to an array in the XML structure.
+ * Link: http://www.bin-co.com/php/scripts/xml2array/
+ * Arguments : $contents - The XML text
+ *                $get_attributes - 1 or 0. If this is 1 the function will get the attributes as well as the tag values - this results in a different array structure in the return value.
+ *                $priority - Can be 'tag' or 'attribute'. This will change the way the resulting array sturcture. For 'tag', the tags are given more importance.
+ * Return: The parsed XML in an array form. Use print_r() to see the resulting array structure.
+ * Examples: $array =  xml2array(file_get_contents('feed.xml'));
+ *              $array =  xml2array(file_get_contents('feed.xml', 1, 'attribute'));
+ */
+function xml2array($contents, $get_attributes=1, $priority = 'tag') {
+    if(!$contents) return array();
+
+    if(!function_exists('xml_parser_create')) {
+        //print "'xml_parser_create()' function not found!";
+        return array();
+    }
+
+    //Get the XML parser of PHP - PHP must have this module for the parser to work
+    $parser = xml_parser_create('');
+    xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8"); # http://minutillo.com/steve/weblog/2004/6/17/php-xml-and-character-encodings-a-tale-of-sadness-rage-and-data-loss
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+    xml_parse_into_struct($parser, trim($contents), $xml_values);
+    xml_parser_free($parser);
+
+    if(!$xml_values) return;//Hmm...
+
+    //Initializations
+    $xml_array = array();
+    $parents = array();
+    $opened_tags = array();
+    $arr = array();
+
+    $current = &$xml_array; //Refference
+
+    //Go through the tags.
+    $repeated_tag_index = array();//Multiple tags with same name will be turned into an array
+    foreach($xml_values as $data) {
+        unset($attributes,$value);//Remove existing values, or there will be trouble
+
+        //This command will extract these variables into the foreach scope
+        // tag(string), type(string), level(int), attributes(array).
+        extract($data);//We could use the array by itself, but this cooler.
+
+        $result = array();
+        $attributes_data = array();
+        
+        if(isset($value)) {
+            if($priority == 'tag') $result = $value;
+            else $result['value'] = $value; //Put the value in a assoc array if we are in the 'Attribute' mode
+        }
+
+        //Set the attributes too.
+        if(isset($attributes) and $get_attributes) {
+            foreach($attributes as $attr => $val) {
+                if($priority == 'tag') $attributes_data[$attr] = $val;
+                else $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+            }
+        }
+
+        //See tag status and do the needed.
+        if($type == "open") {//The starting of the tag '<tag>'
+            $parent[$level-1] = &$current;
+            if(!is_array($current) or (!in_array($tag, array_keys($current)))) { //Insert New tag
+                $current[$tag] = $result;
+                if($attributes_data) $current[$tag. '_attr'] = $attributes_data;
+                $repeated_tag_index[$tag.'_'.$level] = 1;
+
+                $current = &$current[$tag];
+
+            } else { //There was another element with the same tag name
+
+                if(isset($current[$tag][0])) {//If there is a 0th element it is already an array
+                    $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+                    $repeated_tag_index[$tag.'_'.$level]++;
+                } else {//This section will make the value an array if multiple tags with the same name appear together
+                    $current[$tag] = array($current[$tag],$result);//This will combine the existing item and the new item together to make an array
+                    $repeated_tag_index[$tag.'_'.$level] = 2;
+                    
+                    if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
+                        $current[$tag]['0_attr'] = $current[$tag.'_attr'];
+                        unset($current[$tag.'_attr']);
+                    }
+
+                }
+                $last_item_index = $repeated_tag_index[$tag.'_'.$level]-1;
+                $current = &$current[$tag][$last_item_index];
+            }
+
+        } elseif($type == "complete") { //Tags that ends in 1 line '<tag />'
+            //See if the key is already taken.
+            if(!isset($current[$tag])) { //New Key
+                $current[$tag] = $result;
+                $repeated_tag_index[$tag.'_'.$level] = 1;
+                if($priority == 'tag' and $attributes_data) $current[$tag. '_attr'] = $attributes_data;
+
+            } else { //If taken, put all things inside a list(array)
+                if(isset($current[$tag][0]) and is_array($current[$tag])) {//If it is already an array...
+
+                    // ...push the new element into that array.
+                    $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+                    
+                    if($priority == 'tag' and $get_attributes and $attributes_data) {
+                        $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+                    }
+                    $repeated_tag_index[$tag.'_'.$level]++;
+
+                } else { //If it is not an array...
+                    $current[$tag] = array($current[$tag],$result); //...Make it an array using using the existing value and the new value
+                    $repeated_tag_index[$tag.'_'.$level] = 1;
+                    if($priority == 'tag' and $get_attributes) {
+                        if(isset($current[$tag.'_attr'])) { //The attribute of the last(0th) tag must be moved as well
+                            
+                            $current[$tag]['0_attr'] = $current[$tag.'_attr'];
+                            unset($current[$tag.'_attr']);
+                        }
+                        
+                        if($attributes_data) {
+                            $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+                        }
+                    }
+                    $repeated_tag_index[$tag.'_'.$level]++; //0 and 1 index is already taken
+                }
+            }
+
+        } elseif($type == 'close') { //End of tag '</tag>'
+            $current = &$parent[$level-1];
+        }
+    }
+    
+    return($xml_array);
+}  
+
+
+/* returns the shortened url */
+function get_bitly_short_url($url,$login,$appkey,$format='txt') {
+	$connectURL = 'http://api.bit.ly/v3/shorten?login='.$login.'&apiKey='.$appkey.'&uri='.urlencode($url).'&format='.$format;
+	return curl_get_result($connectURL);
+}
+
+/* returns expanded url */
+function get_bitly_long_url($url,$login,$appkey,$format='txt') {
+	$connectURL = 'http://api.bit.ly/v3/expand?login='.$login.'&apiKey='.$appkey.'&shortUrl='.urlencode($url).'&format='.$format;
+	return curl_get_result($connectURL);
+}
+
+/* returns a result form url */
+function curl_get_result($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+
+// A simple function using Curl to post (GET) to Twitter
+// Kosso : March 14 2007
+
+// Feel free to do what you like with this.
+// It's pretty easy. But I thought I'd just put it out there.
+// Curl is your friend. 
+
+// usage :  postToTwitter("myusername","mypassword","hello twitterati! I'm posting this from a PHP script! woo!");
+
+/////////////////////////////////////////////////////////////////////
+
+
+function postToTwitter($username,$password,$message){
+
+    // GET the API url via web authentication
+    // add 'status' param to send a message to Twitter
+
+    $host = "http://twitter.com/statuses/update.xml?status=".urlencode(stripslashes(urldecode($message)));
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $host);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+
+    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+
+    // Go for it!!!
+    $result = curl_exec($ch);
+    // Look at the returned header
+    $resultArray = curl_getinfo($ch);
+
+    // close curl
+    curl_close($ch);
+
+    //echo "http code: ".$resultArray['http_code']."<br />";
+
+    if($resultArray['http_code'] == "200"){
+        echo "<br />OK! posted to http://twitter.com/".$username."/<br />";
+    } else {
+        echo "eek! yegads! error posting to Twitter";
+    }
+
+    // debug the result
+    // echo "<pre>";
+    // print_r($resultArray);
+    // echo "</pre><hr>";
+
+    // $sResult = htmlentities($result);
+    // $sResult = str_replace("&gt;&lt;","&gt;<br />&lt;",$sResult);
+
+    // echo "<pre>";
+    // print $sResult;
+    // echo "</pre>";
+
+} 
+
+
+
+
+
+//bit.ly account
+$bitly_account = 'bit.ly brukernavn';
+$bitly_key = 'bit.ly passord';
+
+//twitter account
+$twitter_account = 'twitter brukernavn';
+$twitter_password = 'twitter passord';
+
+//Todays date formated as yyyymmdd
+$today = date('Ymd');
+
+//Create URL
+//Erstatt p_id med id-en til din kino.
+//kino = JA, ALL or <blank> for hhv kinoforestillinger, begge eller kun teaterforestillinger
+//type = XML, 1 eller 2 for hhv XML, url-encoding eller string separert med pipe/stolpe
+$xmlsource = 'http://www.dx.no/ebillett/dx_forestillinger.php?p_id=206&kino=JA&fra_dato=' . $today . '&til_dato=' . $today . '&type=XML';
+
+//Fetch content and store it in an array
+$contents = file_get_contents($xmlsource);
+$result = xml2array($contents,0);
+//print_r($result);
+
+foreach ($result[Showings][Show] as $show) {
+	$showURL = $show[URLabout];
+	$showTicket =  'http://' . $show[TicketURL];
+	$showMessage = $show[MovieTitle] . ' på kino i dag kl ' . $show[ShowTime] . ' ' . get_bitly_short_url($showURL,$bitly_account,$bitly_key) . 'Billetter: ' . get_bitly_short_url($showTicket,$bitly_account,$bitly_key);
+	postToTwitter($twitter_account,$twitter_password,$showMessage);
+}
+?>
